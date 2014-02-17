7bf6c35ccea85c11aa9ae53a84e0644950c81b95
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2014-02-16 22:41:03 -0500

    Continue adding content and styling, including Font Awesome.

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ var bower = require('gulp-bower');
 var minifyCSS = require('gulp-minify-css');
 
 var paths = {
-  css: 'src/styles/**/*.less'
+  css: 'src/styles/**/*.less',
+  fonts: 'bower_components/font-awesome/fonts/**/*'
 };
 
 gulp.task('bower', function() {
@@ -18,8 +19,12 @@ gulp.task('css', function() {
     .pipe(gulp.dest('public/css'));
 });
 
+gulp.task('fonts', function() {
+  return gulp.src(paths.fonts)
+    .pipe(gulp.dest('public/fonts'));
+});
 gulp.task('watch', function() {
   gulp.watch(paths.css, ['css']);
 });
 
-gulp.task('default', ['bower', 'css']);
+gulp.task('default', ['bower', 'css', 'fonts']);
