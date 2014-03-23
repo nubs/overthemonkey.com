9b402e8434040f73086d79d3057d08572b3ce6f0
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2014-03-22 20:30:58 -0400

    Set the active page in nav correctly.

--- src/config/dispatcher.php
+++ src/config/dispatcher.php
@@ -5,10 +5,10 @@ return function(Container $app) {
     $app->params['Aura\Dispatcher\Dispatcher'] = [
         'objects' => [
             'home' => function() use($app) {
-                return $app->get('templater')->loadTemplate('home.twig')->render([]);
+                return $app->get('templater')->loadTemplate('home.twig')->render(['page' => 'index']);
             },
             'resume' => function() use($app) {
-                return $app->get('templater')->loadTemplate('resume.twig')->render([]);
+                return $app->get('templater')->loadTemplate('resume.twig')->render(['page' => 'resume']);
             },
         ],
         'object_param' => 'action',
